e385cc87faf417d65a39cf9e7c0ca867595fd3e5
LeonSchroijen <76013761+LeonSchroijen@users.noreply.github.com>
2021-04-27 17:34:54 +0200

    Update updateWebSite.php

--- updateWebSite.php
+++ updateWebSite.php
@@ -1,5 +1,5 @@
 <?php
 passthru('echo updating...');
 echo get_current_user();
-passthru('/var/www/leon-schroijen.ga/public_html/deploy.sh');
+echo passthru('/var/www/leon-schroijen.ga/public_html/deploy.sh');
 exit();
